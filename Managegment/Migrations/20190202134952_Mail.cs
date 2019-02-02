e78ea2ab1cf526cacc175c00ae191809e6f0893c
abdullah <abdullah.el_ameer@yahoo.com>
2019-02-03 01:04:37 +0200

    first commit

---
+++ Managegment/Migrations/20190202134952_Mail.cs
@@ -0,0 +1,222 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Managegment.Migrations
+{
+    public partial class Mail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Discriminator = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<long>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: true),
+                    Gender = table.Column<short>(nullable: true),
+                    ModifiedBy = table.Column<long>(nullable: true),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Status = table.Column<short>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conversations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IsEmailEnabled = table.Column<bool>(nullable: false),
+                    IsSMSEnabled = table.Column<bool>(nullable: false),
+                    Subject = table.Column<string>(nullable: true),
+                    TimeStamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conversations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupMembers",
+                columns: table => new
+                {
+                    GroupMemberId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GroupId = table.Column<long>(nullable: true),
+                    MemberId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupMembers", x => x.GroupMemberId);
+                    table.ForeignKey(
+                        name: "FK_GroupMembers_Actors_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_GroupMembers_Actors_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AuthorId = table.Column<long>(nullable: false),
+                    ConversationId = table.Column<long>(nullable: false),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    Payload = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageId);
+                    table.ForeignKey(
+                        name: "FK_Messages_Actors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Messages_Conversations_ConversationId",
+                        column: x => x.ConversationId,
+                        principalTable: "Conversations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participations",
+                columns: table => new
+                {
+                    ConversationId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ConversationId1 = table.Column<long>(nullable: true),
+                    MemberId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participations", x => x.ConversationId);
+                    table.ForeignKey(
+                        name: "FK_Participations_Conversations_ConversationId1",
+                        column: x => x.ConversationId1,
+                        principalTable: "Conversations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Participations_Actors_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MessageTransactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ConvId = table.Column<long>(nullable: false),
+                    IsRead = table.Column<bool>(nullable: false),
+                    MemberId = table.Column<long>(nullable: false),
+                    MessageId = table.Column<long>(nullable: false),
+                    TimeStamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageTransactions", x => x.TransactionId);
+                    table.ForeignKey(
+                        name: "FK_MessageTransactions_Actors_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MessageTransactions_Messages_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "Messages",
+                        principalColumn: "MessageId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupMembers_GroupId",
+                table: "GroupMembers",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupMembers_MemberId",
+                table: "GroupMembers",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_AuthorId",
+                table: "Messages",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ConversationId",
+                table: "Messages",
+                column: "ConversationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MessageTransactions_MemberId",
+                table: "MessageTransactions",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MessageTransactions_MessageId",
+                table: "MessageTransactions",
+                column: "MessageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participations_ConversationId1",
+                table: "Participations",
+                column: "ConversationId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participations_MemberId",
+                table: "Participations",
+                column: "MemberId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GroupMembers");
+
+            migrationBuilder.DropTable(
+                name: "MessageTransactions");
+
+            migrationBuilder.DropTable(
+                name: "Participations");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Conversations");
+        }
+    }
+}
---
+++ Managegment/Models/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Managegment.Models
+{
+    public partial class Users
+    {
+        public long UserId { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public short UserType { get; set; }
+        public string Email { get; set; }
+        public short Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public short LoginTryAttempts { get; set; }
+        public DateTime? LoginTryAttemptDate { get; set; }
+        public DateTime? LastLoginOn { get; set; }
+        public byte[] Photo { get; set; }
+        public long? NationalId { get; set; }
+        public int? PersonId { get; set; }
+        public short Status { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public long CreatedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public long? ModifiedBy { get; set; }
+    }
+}
